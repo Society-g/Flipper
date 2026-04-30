7547d7eb1ff45cbda22bd1c57711530cd7ebb21f
Society_g <cooperstooper00@gmail.com>
2026-04-30 17:12:59 -0500

    Update invite.ps1

--- invite.ps1
+++ invite.ps1
@@ -7,3 +7,4 @@ Start-Sleep -Seconds 5
 [System.Windows.Forms.SendKeys]::SendWait('{ENTER}')
 Start-Sleep -Seconds 1
 [System.Windows.Forms.SendKeys]::SendWait('%{F4}')
+[System.Windows.Forms.SendKeys]::SendWait('%{ENTER}')
