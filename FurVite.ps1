9ca7b8b45f88fbdbe293dc3982555ac372fa8394
Society_g <cooperstooper00@gmail.com>
2026-05-01 09:45:22 -0500

    Add files via upload

---
+++ FurVite.ps1
@@ -0,0 +1,10 @@
+Add-Type -AssemblyName System.Windows.Forms
+$o = New-Object -ComObject WScript.Shell
+$discordInvite = "https://disboard.org/server/join/987721768158502942"
+start-process $discordInvite
+Start-Sleep -Seconds 5
+[System.Windows.Forms.SendKeys]::SendWait('{TAB}')
+[System.Windows.Forms.SendKeys]::SendWait('{ENTER}')
+Start-Sleep -Seconds 1
+[System.Windows.Forms.SendKeys]::SendWait('%{F4}')
+[System.Windows.Forms.SendKeys]::SendWait('%{ENTER}')
